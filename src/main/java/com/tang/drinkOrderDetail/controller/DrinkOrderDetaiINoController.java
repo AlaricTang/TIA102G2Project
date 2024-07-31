095b005b540dec62716f43bf349e3c7736b77c78
Alaric <effortimp@gmail.com>
2024-07-31 17:56:11 +0800

    tang update campaign

--- src/main/java/com/tang/drinkOrderDetail/controller/DrinkOrderDetaiINoController.java
+++ src/main/java/com/tang/drinkOrderDetail/controller/DrinkOrderDetaiINoController.java
@@ -1,15 +1,61 @@
 package com.tang.drinkOrderDetail.controller;
 
 
+import java.io.IOException;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tang.drinkOrder.model.DrinkOrderService;
+import com.tang.drinkOrder.model.DrinkOrderVO;
+import com.tang.drinkOrderDetail.model.DrinkOrderDetailService;
+import com.tang.drinkOrderDetail.model.DrinkOrderDetailVO;
+
+import oracle.jdbc.proxy.annotation.Post;
 
 
 @Controller
-@Validated
 @RequestMapping("/drinkOrderDetail")
 public class DrinkOrderDetaiINoController {
+	
+	@Autowired
+	DrinkOrderDetailService drinkOrderDetailSvc;
+	
+	@Autowired
+	DrinkOrderService drinkOrderSvc;
+	
+//	@GetMapping("addDrinkOrderDetail")
+//	public String addDrinkOrderDetail(Model model) {
+//		DrinkOrderDetailVO deinkOrderDetailVO = new DrinkOrderDetailVO();
+//		model.addAttribute("deinkOrderDetailVO",deinkOrderDetailVO);
+//		return null;
+//	}
+	
+	
+//	//統一前往drinkOrder Controller處理
+//	@PostMapping("insert")
+//	public String insert(@Valid DrinkOrderDetailVO drinkOrderDetailVO, BindingResult result, ModelMap model) throws IOException{
+//		drinkOrderDetailSvc.addDrinkOrderDetail(drinkOrderDetailVO);
+//		
+//		return null;
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 //	
 //	@ExceptionHandler(value = { ConstraintViolationException.class })
