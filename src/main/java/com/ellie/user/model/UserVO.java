8ec79e93c6a857254dae5845570b9cf9bf4c548c
Ellie <vous32bien1@gmail.com>
2024-07-31 15:33:25 +0800

    ellie update UserVO

--- src/main/java/com/ellie/user/model/UserVO.java
+++ src/main/java/com/ellie/user/model/UserVO.java
@@ -1,123 +1,129 @@
 package com.ellie.user.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Date;
 
-@Entity
-@Table(name = "User")
-public class UserVO implements Serializable {
-    private static final long serialVersionUID = 1L;
+	@Entity
+	@Table(name = "User")
+	public class UserVO implements Serializable {
+		private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Integer userId;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "userId")
+		private Integer userId;
 
-    @Column(name = "user_email", nullable = false, unique = true)
-    private String userEmail;
+		@NotEmpty(message = "電子郵件: 請勿空白")
+		@Column(name = "userEmail", nullable = false, unique = true)
+		private String userEmail;
 
-    @Column(name = "user_pwd", nullable = false)
-    private String userPwd;
+		@NotEmpty(message = "密碼: 請勿空白")
+		@Column(name = "userPwd", nullable = false)
+		private String userPwd;
 
-    @Column(name = "user_name", nullable = false)
-    private String userName;
+		@NotEmpty(message = "姓名: 請勿空白")
+		@Pattern(regexp = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,10}$", message = "員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間")
+		@Column(name = "userName", nullable = false)
+		private String userName;
 
-    @Column(name = "user_birth")
-    @Temporal(TemporalType.DATE)
-    private Date userBirth;
+		@Column(name = "userBirth")
+		@Temporal(TemporalType.DATE)
+		private Date userBirth;
 
-    @Column(name = "user_phone")
-    private String userPhone;
+		@Column(name = "userPhone")
+		private String userPhone;
 
-    @Column(name = "user_gender")
-    private Integer userGender;
+		@Column(name = "userGender")
+		private Integer userGender;
 
-    @Column(name = "user_addr")
-    private String userAddr;
+		@Column(name = "userAddr")
+		private String userAddr;
 
-    @Column(name = "user_create_time", nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private Timestamp userCreateTime;
+		@Column(name = "userCreateTime", nullable = false, updatable = false)
+		@Temporal(TemporalType.TIMESTAMP)
+		private Timestamp userCreateTime;
 
+		public Integer getUserId() {
+			return userId;
+		}
 
-    public Integer getUserId() {
-        return userId;
-    }
+		public void setUserId(Integer userId) {
+			this.userId = userId;
+		}
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
+		public String getUserEmail() {
+			return userEmail;
+		}
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+		public void setUserEmail(String userEmail) {
+			this.userEmail = userEmail;
+		}
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
+		public String getUserPwd() {
+			return userPwd;
+		}
 
-    public String getUserPwd() {
-        return userPwd;
-    }
+		public void setUserPwd(String userPwd) {
+			this.userPwd = userPwd;
+		}
 
-    public void setUserPwd(String userPwd) {
-        this.userPwd = userPwd;
-    }
+		public String getUserName() {
+			return userName;
+		}
 
-    public String getUserName() {
-        return userName;
-    }
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+		public Date getUserBirth() {
+			return userBirth;
+		}
 
-    public Date getUserBirth() {
-        return userBirth;
-    }
+		public void setUserBirth(Date userBirth) {
+			this.userBirth = userBirth;
+		}
 
-    public void setUserBirth(Date userBirth) {
-        this.userBirth = userBirth;
-    }
+		public String getUserPhone() {
+			return userPhone;
+		}
 
-    public String getUserPhone() {
-        return userPhone;
-    }
+		public void setUserPhone(String userPhone) {
+			this.userPhone = userPhone;
+		}
 
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
-    }
+		public Integer getUserGender() {
+			return userGender;
+		}
 
-    public Integer getUserGender() {
-        return userGender;
-    }
+		public void setUserGender(Integer userGender) {
+			this.userGender = userGender;
+		}
 
-    public void setUserGender(Integer userGender) {
-        this.userGender = userGender;
-    }
+		public String getUserAddr() {
+			return userAddr;
+		}
 
-    public String getUserAddr() {
-        return userAddr;
-    }
+		public void setUserAddr(String userAddr) {
+			this.userAddr = userAddr;
+		}
 
-    public void setUserAddr(String userAddr) {
-        this.userAddr = userAddr;
-    }
+		public Timestamp getUserCreateTime() {
+			return userCreateTime;
+		}
 
-    public Timestamp getUserCreateTime() {
-        return userCreateTime;
-    }
+		public void setUserCreateTime(Timestamp userCreateTime) {
+			this.userCreateTime = userCreateTime;
+		}
 
-    public void setUserCreateTime(Timestamp userCreateTime) {
-        this.userCreateTime = userCreateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "UserVO [userId=" + userId + ", userEmail=" + userEmail + ", userPwd=" + userPwd + ", userName="
-                + userName + ", userBirth=" + userBirth + ", userPhone=" + userPhone + ", userGender=" + userGender
-                + ", userAddr=" + userAddr + ", userCreateTime=" + userCreateTime + "]";
-    }
-}
+		@Override
+		public String toString() {
+			return "UserVO [userId=" + userId + ", userEmail=" + userEmail + ", userPwd=" + userPwd + ", userName="
+					+ userName + ", userBirth=" + userBirth + ", userPhone=" + userPhone + ", userGender=" + userGender
+					+ ", userAddr=" + userAddr + ", userCreateTime=" + userCreateTime + "]";
+		}
+	}
