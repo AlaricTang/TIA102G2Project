de1ec9eb68a551109c9db0d5edeffce05f565294
yishi <yishi@gmail.com>
2024-08-01 11:04:19 +0800

    LI's commit

---
+++ src/main/java/com/LI/Customer/model/CustomerDAO_interface.java
@@ -0,0 +1,17 @@
+package com.LI.Customer.model;
+
+import java.util.List;
+
+public interface CustomerDAO_interface {
+	
+	public void insert(CustomerVO customerVO);
+	public void update(CustomerVO customerVO);
+	public void delete(Integer customer_id);
+	
+	public CustomerVO findByPrimaryKey(Integer customer_id);
+	
+	public List<CustomerVO> getAll();
+
+}
+
+
