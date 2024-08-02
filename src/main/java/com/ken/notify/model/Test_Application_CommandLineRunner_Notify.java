c9d7e566949adb537da8fcb9fd6ea5a99a78b067
Ken Tung <ken.gastest@gmail.com>
2024-08-02 17:13:25 +0800

    ken cupRecordVO 創建 註解notify test applicaiton

--- src/main/java/com/ken/notify/model/Test_Application_CommandLineRunner_Notify.java
+++ src/main/java/com/ken/notify/model/Test_Application_CommandLineRunner_Notify.java
@@ -2,7 +2,7 @@
 
 //import java.util.List;
 //import java.util.Optional;
-//
+
 //import org.hibernate.SessionFactory;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.CommandLineRunner;
