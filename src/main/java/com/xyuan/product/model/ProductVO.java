5350acb79bda97f44aa071a5187a923e153f0240
13_joanne <yuan1930@gmail.com>
2024-08-02 16:13:53 +0800

    xyuan update ProductVO

--- src/main/java/com/xyuan/product/model/ProductVO.java
+++ src/main/java/com/xyuan/product/model/ProductVO.java
@@ -4,12 +4,17 @@ import java.io.Serializable;
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+
+@Entity
+@Table(name="product")
 public class ProductVO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
