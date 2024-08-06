80f0200aa8f1215e5737ea239f8675b1c2bdd653
Ellie <vous32bien1@gmail.com>
2024-08-06 16:41:00 +0800

    ellie upload FavoriteService&controller

---
+++ src/main/java/com/redis/favorite/FavoriteService.java
@@ -0,0 +1,40 @@
+package com.redis.favorite;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.redis.JedisService;
+
+
+@Service
+public class FavoriteService {
+	
+	@Autowired
+	JedisService jedisSvc;
+
+
+	    private static final String FAVORITE_KEY_PREFIX = "favorite:";
+
+	    // 儲存收藏
+	    public void addFavorite(Integer userID, Integer productID) throws IOException {
+	        String key = FAVORITE_KEY_PREFIX + userID;
+	        jedisSvc.saveUserOneOne(key, productID.toString(), productID.toString());
+	    }
+
+	    // 會員所有收藏的商品
+	    public List<Object> getFavorites(Integer userID) throws IOException {
+	        String key = FAVORITE_KEY_PREFIX + userID;
+	        return jedisSvc.getList(key);
+	    }
+
+	    // 刪除收藏
+	    public void removeFavorite(Integer userID, Integer productID) throws IOException {
+	        String key = FAVORITE_KEY_PREFIX + userID;
+	        jedisSvc.delete(key);
+	    }
+
+	
+	}
+
